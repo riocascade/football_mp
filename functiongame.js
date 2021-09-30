d9d9e947a973b874ee1d6c7e218db72fbe08fa67
riocascade <riocascade@gmail.com>
2021-09-30 22:11:38 +0700

    first

---
+++ functiongame.js
@@ -0,0 +1,11 @@
+
+function setTimer() {
+    c2_callFunction("setTimer", [120]);//call function "setTimer" in game with parameter 0 as value in seconds
+    //c2_callFunction("deleteFruit"<?php echo json_encode($my_var); ?>);//call function "deleteFruit" in game. this function to reset total fruit in game
+    
+    }
+function getScore(x) {
+    var totalScore = x ;//get score by element in game
+    console.log('total Score '+totalScore);//log
+    
+    };
