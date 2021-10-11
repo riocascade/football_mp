d0c1897cd955a503ec94ad7a796c267bb30c2e55
riocascade <riocascade@gmail.com>
2021-10-11 20:57:11 +0700

    redirect if both dc

--- functiongame.js
+++ functiongame.js
@@ -4,12 +4,12 @@ function setTimer() {
     //c2_callFunction("deleteFruit"<?php echo json_encode($my_var); ?>);//call function "deleteFruit" in game. this function to reset total fruit in game
     
     }
-function getScore(x) {
+function getScore(x, userid) {
     var totalScore = x ;//get score by element in game
     console.log('total Score '+totalScore);//log
-    
+	
+	window.setTimeout(
+	function(){ window.location = "http://www.yoururl.com"; },3000
+	); 
     };
 	
-function onDisconect() {
-
-};
